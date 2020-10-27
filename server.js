f2e25210a8d1ed7dcc901680543d97502a0fdde4
sunjay_local <65554107+sarmstead@users.noreply.github.com>
2020-10-27 18:13:41 -0400

    Create, export, import, and use middleware functions for Express

--- server.js
+++ server.js
@@ -1,9 +1,6 @@
 const express = require('express');
 const app = express();
 
-const bodyParser = require('body-parser');
-const cors = require('cors');
-const errorHandler = require('errorhandler');
-const morgan = require('morgan');
+app.use(require('./middleware'));
 
 const PORT = process.env.PORT || 3000;
