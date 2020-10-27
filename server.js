d2ed68ef899cbd721e309030ef9d15b37bd410e6
sunjay_local <65554107+sarmstead@users.noreply.github.com>
2020-10-27 17:55:09 -0400

    Create PORT logic

--- server.js
+++ server.js
@@ -5,3 +5,5 @@ const bodyParser = require('body-parser');
 const cors = require('cors');
 const errorHandler = require('errorhandler');
 const morgan = require('morgan');
+
+const PORT = process.env.PORT || 3000;
