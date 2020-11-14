ce95ab2a6139acceef0dce948e4f56186962ca26
sunjay_local <65554107+sarmstead@users.noreply.github.com>
2020-11-14 14:11:44 -0500

    Create Artist table if it does not already exist

--- migration.js
+++ migration.js
@@ -1,2 +1,10 @@
 const sqlite3 = require('sqlite3');
 const db = new sqlite3.Database('./database.sqlite');
+
+db.run('CREATE TABLE IF NOT EXISTS `Artist` ( ' +
+    '`id` INTEGER NOT NULL, ' +
+    '`name` TEXT NOT NULL, ' +
+    '`date_of_birth` TEXT NOT NULL, ' +
+    '`biography` TEXT NOT NULL, ' +
+    '`is-currently_employed` INTEGER NOT NULL DEFAULT 1, ' +
+    'PRIMARY KEY(`id`) )');
