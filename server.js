70785ec1a86ccef8a5c22a788238f040d546d119
sunjay_local <65554107+sarmstead@users.noreply.github.com>
2020-11-14 13:43:53 -0500

    Start server. Add helpful comments.

--- server.js
+++ server.js
@@ -1,6 +1,13 @@
 const express = require('express');
 const app = express();
 
+// Require middleware functions for every route
 app.use(require('./middleware'));
 
+// Create PORT
 const PORT = process.env.PORT || 3000;
+
+// Start server
+app.listen(PORT, () => {
+    console.log(`Listening on port: ${PORT}`);
+});
