4552c4d0e01e31e1a59736906a10b8b61215e894
sunjay_local <65554107+sarmstead@users.noreply.github.com>
2020-11-15 20:47:40 -0500

    Migrate middleware function to server file

--- server.js
+++ server.js
@@ -2,8 +2,18 @@ const express = require('express');
 const app = express();
 const apiRouter = require('./api/api');
 
+// Import middleware
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const errorHandler = require('errorhandler');
+const morgan = require('morgan');
+
 // Require middleware functions for every route
-app.use(require('./middleware'));
+app.use(bodyParser.json());
+app.use(cors());
+app.use(errorHandler());
+app.use(morgan('dev'));
+
 
 // Mount API router at all routes starting at /api
 app.use('/api', apiRouter);
