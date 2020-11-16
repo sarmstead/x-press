895c275281b6ec4a847829cd58f1494f7cb89ee5
sunjay_local <65554107+sarmstead@users.noreply.github.com>
2020-11-15 20:01:37 -0500

    Add :artistId param to reduce boilerplate code

--- api/artists.js
+++ api/artists.js
@@ -1,10 +1,25 @@
 const express = require('express');
 const artistsRouter = express.Router();
 const sqlite3 = require('sqlite3');
-const db = new sqlite3.Database(process.env.TEST_DATABASE || './database.sqlite'); 
+const db = new sqlite3.Database(process.env.TEST_DATABASE || '../database.sqlite'); 
+
+artistsRouter.param('artistId', (req, res, next, artistId) => {
+    const sql = 'SELECT * FROM Artist WHERE Artist.id = $artistId';
+    const values = {$artistId: artistId};
+    db.get(sql, values, (err, artist) => {
+        if (err) {
+            next(err);
+        } else if (artist) {
+            req.artist = artist;
+            next();
+        } else {
+            res.sendStatus(404);
+        }
+    });
+});
 
 artistsRouter.get('/', (req, res, next) => {
-    db.all('SELECT * FROM Artist WHERE is_currently_employed == 1', (err, artists) => {
+    db.all('SELECT * FROM Artist WHERE is_currently_employed = 1', (err, artists) => {
         if (err) {
             next(err);
         } else {
