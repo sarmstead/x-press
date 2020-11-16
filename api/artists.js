b2989bb2b86ec30ccff381d595e837f20d027b37
sunjay_local <65554107+sarmstead@users.noreply.github.com>
2020-11-15 19:39:40 -0500

    Add Artist GET route

--- api/artists.js
+++ api/artists.js
@@ -3,4 +3,14 @@ const artistsRouter = express.Router();
 const sqlite3 = require('sqlite3');
 const db = new sqlite3.Database(process.env.TEST_DATABASE || './database.sqlite'); 
 
+artistsRouter.get('/', (req, res, next) => {
+    db.all('SELECT * FROM Artist WHERE is_currently_employed == 1', (err, artists) => {
+        if (err) {
+            next(err);
+        } else {
+            res.status(200).json({artists: artists});
+        }
+    });
+});
+
 module.exports = artistsRouter;
