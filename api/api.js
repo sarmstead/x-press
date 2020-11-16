2b54ded257b28bf6f93cda6fa5311b744a2b0da9
sunjay_local <65554107+sarmstead@users.noreply.github.com>
2020-11-15 19:29:09 -0500

    Create Artist routes file, exports, and imports

--- api/api.js
+++ api/api.js
@@ -1,4 +1,8 @@
 const express = require('express');
+const app = require('../server');
 const apiRouter = express.Router();
+const artistsRouter = require('./artists');
+
+app.use('/artists', artistsRouter);
 
 module.exports = apiRouter;
