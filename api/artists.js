9da4226a4fd2e0f462698f7bf8e5496f65eb2a7d
sunjay_local <65554107+sarmstead@users.noreply.github.com>
2020-11-17 09:10:39 -0500

    Create POST route for /api/artists

--- api/artists.js
+++ api/artists.js
@@ -33,4 +33,32 @@ artistsRouter.get('/:artistId', (req, res, next) => {
     res.status(200).json({artist: req.artist});
 });
 
+artistsRouter.post('/', (req, res, next) => {
+    const name = req.body.artist.name;
+    const dateOfBirth = req.body.artist.dateOfBirth;
+    const biography = req.body.artist.biography;
+    if (!name || !dateOfBirth || !biography) {
+        res.sendStatus(400);
+    } 
+
+    const isCurrentlyEmployed = req.body.artist.isCurrentlyEmployed === 0 ? 0 : 1;
+
+    const sql = 'INSERT INTO Artist (name, date_of_birth, biography, is_currently_employed) VALUES ($name, $dateOfBirth, $biography, $isCurrentlyEmployed)';
+    const values = {
+        $name: name,
+        $dateOfBirth: dateOfBirth,
+        $biography: biography,
+        $isCurrentlyEmployed: isCurrentlyEmployed
+    }
+    db.run(sql, values, function(err) {
+        if (err) {
+            next(err);
+        } else {
+            db.get(`SELECT * FROM Artist WHERE Artist.id = ${this.lastID}`, (err2, artist) => {
+                res.status(201).json({artist: artist});
+            })
+        }
+    });
+})
+
 module.exports = artistsRouter;
