7e2901c0cd79a567ab33149fef729285520f91a3
sunjay_local <65554107+sarmstead@users.noreply.github.com>
2020-11-25 16:34:27 -0500

    Import sqlite3 and instantiate a database

--- api/issues.js
+++ api/issues.js
@@ -1,4 +1,11 @@
 const express = require('express');
 const issuesRouter = express.Router({ mergeParams: true });
+const sqlite3 = require('sqlite3');
+db = new sqlite3.Database(process.env.TEST_DATABASE || './database.sqlite');
+
+
+issuesRouter.get('/', (req, res, next) {
+    
+});
 
 module.exports = issuesRouter;
