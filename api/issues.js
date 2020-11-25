4fc87d7075cf6fa211d1860a0efc81040e1e011f
sunjay_local <65554107+sarmstead@users.noreply.github.com>
2020-11-25 16:48:36 -0500

    Add GET route for /:seriesId/issues

--- api/issues.js
+++ api/issues.js
@@ -4,8 +4,17 @@ const sqlite3 = require('sqlite3');
 db = new sqlite3.Database(process.env.TEST_DATABASE || './database.sqlite');
 
 
-issuesRouter.get('/', (req, res, next) {
-    
+issuesRouter.get('/', (req, res, next) => {
+    const sql = 'SELECT * FROM Issue WHERE Issue.series_id = $seriesId';
+    const values = {
+        $seriesId: req.params.seriesId
+    }
+    db.all(sql, values, (err, issues) => {
+        if (err) {
+            next(err);
+        }
+        res.status(200).json({issues: issues})
+    });
 });
 
 module.exports = issuesRouter;
