f9b7ea0fdc4e860d80efb4abde07f44965c525f8
sunjay_local <65554107+sarmstead@users.noreply.github.com>
2020-11-25 14:36:12 -0500

    Move router param above initial GET route. Put space after if statement. Add next() in else if block.

--- api/series.js
+++ api/series.js
@@ -4,6 +4,24 @@ const seriesRouter = express.Router();
 const sqlite3 = require('sqlite3');
 db = new sqlite3.Database(process.env.TEST_DATABASE || './database.sqlite');
 
+seriesRouter.param('seriesId', (req, res, next, seriesId) => {
+    const sql = 'SELECT * FROM Series WHERE Series.id = $seriesId';
+    const values = {
+        $seriesId: seriesId
+    };
+    db.get(sql, values, (err, series) => {
+        if (err) {
+            next(err);
+        } else if (series) {
+            req.series = series;
+            next();
+        } else {
+            res.sendStatus(404);
+        }
+
+    });
+});
+
 seriesRouter.get('/', (req, res, next) => {
     db.all(`SELECT * FROM Series`, (err, series) => {
         if (err) {
@@ -13,22 +31,5 @@ seriesRouter.get('/', (req, res, next) => {
     });
 });
 
-
-seriesRouter.param('seriesId', (req, res, next, seriesId) => {
-    const sql = 'SELECT * FROM Series WHERE Series.id = $seriesId';
-    const values = {
-        $seriesId: seriesId
-    };
-    db.get(sql, values, (err, series) => {
-        if(err) {
-            next(err);
-        } else if (series) {
-            req.series = series;
-        } else {
-            res.sendStatus(404);
-        }
-
-    });
-});
 
 module.exports = seriesRouter;
