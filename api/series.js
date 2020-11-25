9e264f18abe5db0d69693af93900c970b37df35c
sunjay_local <65554107+sarmstead@users.noreply.github.com>
2020-11-25 14:08:43 -0500

    Open a sqlite 3 database using the test database or production database

--- api/series.js
+++ api/series.js
@@ -1,4 +1,7 @@
 const express = require('express');
 const seriesRouter = express.Router();
+const sqlite3 = require('sqlite3');
+db = new sqlite3.Database(process.env.TEST_DATABASE || './database.sqlite');
+
 
 module.exports = seriesRouter;
