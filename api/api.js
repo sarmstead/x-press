90a592ccc464f6a2b5edfef1fa857ba3168aaf63
sunjay_local <65554107+sarmstead@users.noreply.github.com>
2020-11-25 14:05:48 -0500

    Create seriesRouter, export / import it into api.js and mount it at /series route

--- api/api.js
+++ api/api.js
@@ -2,7 +2,9 @@ const express = require('express');
 const app = require('../server');
 const apiRouter = express.Router();
 const artistsRouter = require('./artists');
+const seriesRouter = require('./series');
 
 apiRouter.use('/artists', artistsRouter);
+apiRouter.use('/series', seriesRouter);
 
 module.exports = apiRouter;
