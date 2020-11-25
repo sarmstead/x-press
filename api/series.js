46ed8142b428130eb673363ee3c12b6f62190cd1
sunjay_local <65554107+sarmstead@users.noreply.github.com>
2020-11-25 14:16:47 -0500

    Add GET route to retrieve all series

--- api/series.js
+++ api/series.js
@@ -3,5 +3,14 @@ const seriesRouter = express.Router();
 const sqlite3 = require('sqlite3');
 db = new sqlite3.Database(process.env.TEST_DATABASE || './database.sqlite');
 
+seriesRouter.get('/', (req, res, next) => {
+    db.all(`SELECT * FROM Series`, (err, series) => {
+        if (err) {
+            next(err);
+        }
+        res.status(200).json({series: series});
+    });
+});
+
 
 module.exports = seriesRouter;
