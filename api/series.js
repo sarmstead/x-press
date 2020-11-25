56e0cc4a23bb20a60dd12b7454f2683bff628292
sunjay_local <65554107+sarmstead@users.noreply.github.com>
2020-11-25 14:39:47 -0500

    Add /:seriesId GET route.

--- api/series.js
+++ api/series.js
@@ -31,5 +31,8 @@ seriesRouter.get('/', (req, res, next) => {
     });
 });
 
+seriesRouter.get('/:seriesId', (req, res, next) => {
+    res.status(200).json({series: req.series});
+});
 
 module.exports = seriesRouter;
